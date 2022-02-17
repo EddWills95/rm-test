c95accc9ecc0a058c8230afa5f3921b721312bfb
Edd Williams <edd.williams@me.com>
2022-02-17 11:48:38 +0000

    add swr and data fetching

--- src/components/PropertyListing/PropertyListing.js
+++ src/components/PropertyListing/PropertyListing.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import PropertyCard from '../PropertyCard';
-import './PropertyListing.scss';
+import React from "react";
+import PropertyCard from "../PropertyCard";
+import "./PropertyListing.scss";
 
-const DUMMY_PROPERTY = {
-    id: 73864112,
-    bedrooms: 3,
-    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
-    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
-    propertyType: 'Flat',
-    price: 1950000,
-    branchName: 'M2 Property, London',
-    propertyUrl: '/property-for-sale/property-73864112.html',
-    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
-    propertyTitle: '3 bedroom flat for sale',
-    mainImage: 'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg'
-};
+import useSWR from "swr";
+
+const API_URL = "http://localhost:3000/api/properties";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const PropertyListing = () => {
-    return (
-        <div className="PropertyListing">
-            {
-                Array(5).fill(DUMMY_PROPERTY)
-                    .map((property, index) => <PropertyCard key={index} {...property}/>)
-            }
-        </div>
-    )
+  const { data, error } = useSWR(API_URL, fetcher);
+
+  if (error) return <h1>Sorry, there has been an error fetching properties</h1>;
+  if (!data) return <h1>Loading...</h1>;
+
+  // Could really do with some pagination for performance
+
+  return (
+    <div className="PropertyListing">
+      {data.map((property, index) => (
+        <PropertyCard key={index} {...property} />
+      ))}
+    </div>
+  );
 };
 
 export default PropertyListing;
